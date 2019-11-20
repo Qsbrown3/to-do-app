5e9398b9aff703a3c9d0324efe48f842183676da
Qsbrown3 <qsbrown3@gmail.com>
2019-11-19 18:33:33 -0600

    update JS

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
 function onReady() {
-  const addToDoForm = document.getElementbyId('addToDoForm');
-  const newToDoText = document.getElementbyId('newToDoText');
-  const toDoList = document.getElementbyId('toDoList');
+  let toDos = [];
+
+  const ADD_TODO_FORM = document.getElementbyId('addToDoForm');
+  const NEW_TODO_TEXT = document.getElementbyId('newToDoText');
+  const TODO_LIST = document.getElementbyId('toDoList');
 
 
-  addToDoForm.addEventListener('submit', event => {
+  ADD_TODO_FORM.addEventListener('submit', (event) => {
     event.preventDefault();
 
     // get the text
-    let title = newToDoText.value;
+    let title = NEW_TODO_TEXT.value;
+
+    toDos.push(title);
 
     // create a new li
     let newLi = document.createElement('li');
 
     //  create a new input
     let checkbox = document.createElement('input');
-
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener('click' function(event) {
+      let buttonLiText = this.parentElement.childNodes[0].textContent;
+
+      TODO_LIST.removeChild(this.parentElement);
+
+      toDos.forEach(function(currentToDo, index){
+        if(currentToDo === buttonLiText){
+          toDos.splice(index, 1);
+        }
+      });
+    })
+
+
+
     // set the title
-    newLi.textContent
+    newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
 
+    newLi.appendChild(deleteBtn);
+
     // attach the li to the ul
-    toDoList.appendChild(newLi);
+    TODO_LIST.appendChild(newLi);
 
     // empty the input
-    newToDoText.value = '';
+    NEW_TODO_TEXT.value = "";
+
   });
-}
 
-function deleteListItems() {
-  const deleteButton = document.getElementbyId('addDeleteButton');
-
-  deleteButton.addEventListener('submit', event => {
-    event.preventDefault();
-
-    for (var i = 0; i < deleteButton.length; i++) {
-      deleteButton[i].addEventListener('submit', deleteListItems);
-    }
-  })
-
-}
+};
 
 window.onload = function() {
   onReady();
