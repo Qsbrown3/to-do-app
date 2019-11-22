6b1d81bd432c9fa8776e34bb1a0b56568a9cdc5b
Qsbrown3 <qsbrown3@gmail.com>
2019-11-21 20:41:25 -0600

    Update app-branch1

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 function onReady() {
-  let toDos = [];
 
   const ADD_TODO_FORM = document.getElementById('addToDoForm');
   const NEW_TODO_TEXT = document.getElementById('newToDoText');
@@ -11,41 +10,18 @@ function onReady() {
 
     // get the text
     let title = NEW_TODO_TEXT.value;
-
-    toDos.push(title);
-
     // create a new li
     let newLi = document.createElement('li');
-
     //  create a new input
     let checkbox = document.createElement('input');
     // set the input's type to checkbox
     checkbox.type = "checkbox";
-
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.addEventListener('click', function(event) {
-      let buttonLiText = this.parentElement.childNodes[0].textContent;
-
-      TODO_LIST.removeChild(this.parentElement);
-
-      toDos.forEach(function(currentToDo, index){
-        if(currentToDo === buttonLiText){
-          toDos.splice(index, 1);
-        }
-      });
-    })
-
-
-
+    
     // set the title
     newLi.textContent = title;
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
-
-    newLi.appendChild(deleteBtn);
 
     // attach the li to the ul
     TODO_LIST.appendChild(newLi);
